cd406452a1acccd5b4ee6723e897c52a6f648cf9
vjimenezs <victoria.jimenez@uc.cl>
2023-09-13 01:47:15 +0000

    feat(instructions): Mejora de los pasos con íconos relevantes e indicaciones

--- src/instructions/Step.jsx
+++ src/instructions/Step.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './Step.css';
 
-function Step({ id, title, subtitle, instructions, current }) {
+function Step({ id, title, subtitle, instructions, icons, indication, current }) {
     return (
         <>{current === id &&
             <section className="step">
                 <h1>{title}</h1>
                 <h2>{subtitle}</h2>
                 <p>{instructions}</p>
+                <div className="icon-row">
+                    {icons.map((icon, index) => (
+                        <img
+                            key={index}
+                            src={icon}
+                            className="icon"
+                            alt={`Step ${id}, Icon ${index + 1}`}
+                        />
+                    ))}
+                </div>
+                <p><i>{indication}</i></p>
             </section>
         }</>
     );
